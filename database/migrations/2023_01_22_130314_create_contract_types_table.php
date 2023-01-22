1bac5d1ce38b3e54c771d40cf957555c9b3ff8af
HAC22 <deampuleadd@yandex.com>
2023-01-22 15:29:00 +0100

    [REMOVES] dg column "days of service"

--- database/migrations/2023_01_22_130314_create_contract_types_table.php
+++ database/migrations/2023_01_22_130314_create_contract_types_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('contract_type');
             $table->integer('min_hours_per_shift');
             $table->integer('max_hours_per_week');
-            $table->integer('days_of_service_per_week');
             $table->integer('max_days_of_vacation_per_year');
             $table->timestamps();
         });
