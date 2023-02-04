9a86e61b3314f5db0977ebe939036f752486fbac
HAC22 <deampuleadd@yandex.com>
2023-02-04 17:30:24 +0100

    [BUGFIX] fixes checkbox checked status

--- resources/views/preparation/contracts/create.blade.php
+++ resources/views/preparation/contracts/create.blade.php
@@ -80,7 +80,7 @@
                 </div>
 
                 <label class="relative inline-flex items-center mb-4 cursor-pointer">
-                    <input type="checkbox" value="{{old('break_included')}}" name="break_included" id="break_included" class="sr-only peer">
+                    <input type="checkbox" @if (old('break_included') == "on") checked @endif name="break_included" id="break_included" class="sr-only peer">
                     <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600"></div>
                     <span class="ml-3 text-sm font-medium text-gray-900 dark:text-white">Shift includes break</span>
                 </label>
