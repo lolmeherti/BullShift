9968664c29c16495601ab90538d9ac6fabb81ced
HAC22 <deampuleadd@yandex.com>
2023-02-11 19:06:50 +0100

    [ADD] department controller

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Department;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function index(): Application|Factory|View
+    {
+        //
+        $departments = $this->getAllDepartments();
+        return view('preparation.departments.index', compact('departments'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function create(): View|Factory|Application
+    {
+        //
+        $contractTypes = [''];
+        return view('preparation.departments.create', compact('contractTypes'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Department  $department
+     * @return Response
+     */
+    public function show(Department $department)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Department  $department
+     * @return Response
+     */
+    public function edit(Department $department)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Department  $department
+     * @return Response
+     */
+    public function update(Request $request, Department $department)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Department  $department
+     * @return Response
+     */
+    public function destroy(Department $department)
+    {
+        //
+    }
+
+    public function getAllDepartments(): \Illuminate\Support\Collection
+    {
+        return DB::table('departments')->get();
+    }
+}
