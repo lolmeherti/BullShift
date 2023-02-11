a30a76e8c3557be880830be552fcc2029c535ffe
HAC22 <deampuleadd@yandex.com>
2023-02-11 19:05:14 +0100

    [ADD] link to flowbite js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,9 @@
         }
     </style>
 
+    <!--FLOWBITE-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -65,5 +68,8 @@
         </div>
     </div>
 </div>
+
+<!--FLOWBITE-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
 </body>
 </html>
