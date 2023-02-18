7c4013a32084db13fafaacb6aee78940bd153747
HAC22 <deampuleadd@yandex.com>
2023-02-18 20:26:15 +0100

    [ADDS] the search functionality now ties to the department views

--- resources/views/preparation/departments/create.blade.php
+++ resources/views/preparation/departments/create.blade.php
@@ -16,7 +16,7 @@
                 <p class="text-xl font-semibold">
                     Create a Department
 
-                <ul id="creation_text" class="pt-2 hidden">
+                <ul id="loading_spinner_animated" class="pt-2 hidden">
                     <li class="flex items-center">
                         <svg aria-hidden="true" class="inline w-5 h-5 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-green-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
@@ -68,18 +68,11 @@
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                         </div>
-                        <input type="text" value="" id="input-group-search" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-purple-500 focus:border-purple-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500" placeholder="Search user">
+                        <input type="text" id="inputSearchManagerName" onfocus="this.select()" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-purple-500 focus:border-purple-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500" placeholder="Employee">
                     </div>
                 </div>
-                <ul class="h-48 py-2 overflow-y-auto text-gray-700 dark:text-gray-200" aria-labelledby="dropdownUsersButton">
-                    @foreach($users as $user)
-                        <li>
-                            <a href="#" class="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onclick="setManagerName('{{$user->name}}', {{$user->id}})">
-                                <img class="w-6 h-6 mr-2 rounded-full" src="{{asset('images/1676204359.jpg')}}" alt="profile_picture_{{$user->name}}">
-                                {{$user->name}}
-                            </a>
-                        </li>
-                    @endforeach
+                <ul class="h-48 py-2 overflow-y-auto text-gray-700 dark:text-gray-200" aria-labelledby="dropdownUsersButton" id="usersFound">
+                    <li class="text-center text-gray-400">Start typing to begin searching.</li>
                 </ul>
                 <a href="#" class="flex items-center p-3 text-sm font-medium text-purple-500 border-t border-gray-200 rounded-b-lg bg-gray-50 dark:border-gray-600 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-white hover:underline">
                     <svg class="w-5 h-5 mr-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"></path></svg>
@@ -92,9 +85,9 @@
             </div>
 
             <div style="justify-self: end;" id="button" class="col-start-2 col-end-3 justify-items-end py-4">
-                <button type="submit" onclick="showCreatingDepartmentInProgress()"
+                <button type="submit" id="create_button" onclick="showCreatingDepartmentInProgress()"
                         class="px-5 py-1.5 relative rounded group overflow-hidden font-medium bg-green-600 text-purple-50 inline-block">
-                    <span
+                    <span id="create_button_highlighted"
                         class="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-green-500 group-hover:h-full opacity-90"></span>
                     <span class="relative group-hover:text-white">Submit</span>
                 </button>
@@ -105,24 +98,25 @@
 </x-app-layout>
 
 <script>
-    let showCreatingDepartmentInProgress = () =>
-    {
-
-        let fieldValue = document.getElementById('department').value;
-
-        if(fieldValue)
-        {
-            document.getElementById('creation_text').classList.remove('hidden');
-            document.getElementById('creation_success_message').innerHTML = "";
-        }
-    }
-
     let setManagerName = (name, id) =>
     {
         document.getElementById('manager_user_fid').value = id;
         document.getElementById('manager_name').value = name;
         document.getElementById('manager_name_display').innerHTML = name;
     }
+
+    let delayTimer;
+    const list = document.getElementById("usersFound");
+    let searchUrl = '/department/search/manager';
+
+    document.getElementById("inputSearchManagerName").addEventListener("keyup", function(){
+        clearTimeout(delayTimer); // clear the previous timer
+        let searchQuery = document.getElementById("inputSearchManagerName").value;
+
+        delayTimer = setTimeout(function() {
+            searchForManagerByUserName(searchUrl, searchQuery);
+        }, 700); // wait for 100 milliseconds before sending the request
+    });
 </script>
 
 
