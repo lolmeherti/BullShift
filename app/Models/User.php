3c1650ca629e932bb5654f273a869520c85b52ea
HAC22 <deampuleadd@yandex.com>
2023-02-25 21:05:29 +0100

    [ADDS] property annotations to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+
+/**
+ * @property mixed $name
+ * @property mixed $email
+ * @property mixed $password
+ * @property mixed $email_verified_at
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
