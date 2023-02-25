749d47d1e16dbdc00cd9fa14c26b4200da5bbbe4
HAC22 <deampuleadd@yandex.com>
2023-02-25 21:02:05 +0100

    [ADDS] filled migration for employee table

--- database/migrations/2023_02_19_132900_create_employees_table.php
+++ database/migrations/2023_02_19_132900_create_employees_table.php
@@ -11,10 +11,15 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->tinyInteger('user_fid');
+            $table->tinyInteger('contract_type_fid');
+            $table->tinyInteger('designation_fid');
+            $table->tinyInteger('department_fid')->nullable();
+            $table->decimal('wage_per_year')->nullable();
             $table->timestamps();
         });
     }
@@ -24,7 +29,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('employees');
     }
