00592b9b12fc5f696407e42da406d61887a3be9e
HAC22 <deampuleadd@yandex.com>
2023-02-25 21:04:29 +0100

    [ADDS] properties to employee availability model

--- app/Models/EmployeeAvailability.php
+++ app/Models/EmployeeAvailability.php
@@ -5,6 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * @property int $user_fid
+ * @property int $contract_type_fid
+ * @property string $availability_status
+ * @property float $hours_worked_per_day
+ * @property float $max_hours_per_day
+ * @property float $hours_worked_per_week
+ * @property float $max_hours_per_week
+ * @property float $hours_worked_this_month
+ * @property float $max_hours_this_month
+ * @property int $days_of_vacation_left
+ * @property int $max_days_of_vacation
+ */
 class EmployeeAvailability extends Model
 {
     use HasFactory;
@@ -16,8 +30,8 @@ class EmployeeAvailability extends Model
      */
     protected $fillable = [
         'user_fid',
-        'availability_status',
         'contract_type_fid',
+        'availability_status',
         'hours_worked_per_day',
         'max_hours_per_day',
         'hours_worked_per_week',
