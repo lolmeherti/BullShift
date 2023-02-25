0e85c4634d364d6f6a5236c202ad44952dd549e5
HAC22 <deampuleadd@yandex.com>
2023-02-25 21:03:55 +0100

    [ADDS] properties to employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,7 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $user_fid
+ * @property int $contract_type_fid
+ * @property int $designation_fid
+ * @property int $department_fid
+ * @property float $wage_per_year
+ */
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_fid',
+        'contract_type_fid',
+        'designation_fid',
+        'department_fid',
+        'wage_per_year',
+    ];
 }
