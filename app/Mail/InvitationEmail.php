742178d1766bf2562909d39736e11e6a20d99acf
HAC22 <deampuleadd@yandex.com>
2023-02-26 19:27:29 +0100

    [ADDS] invitation mail controller and template v0.1

---
+++ app/Mail/InvitationEmail.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class InvitationEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public string $email;
+    public string $pw;
+    public string $name;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($email, $pw, $name)
+    {
+        //
+        $this->email = $email;
+        $this->pw = $pw;
+        $this->name = $name;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Invitation Email',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.invitation-email-template.invitation-email',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
---
+++ resources/views/emails/invitation-email-template/invitation-email.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to our application!</title>
+</head>
+<body>
+<h1>Welcome to our application!</h1>
+<p>Dear {{ $name }}, thank you for signing up. We hope you enjoy using our app!</p>
+<p>Your login credentials are:</p>
+<p>{{ $email }}</p>
+<p>{{ $pw }}</p>
+</body>
+</html>
