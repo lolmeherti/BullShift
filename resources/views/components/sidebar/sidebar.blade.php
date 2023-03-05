c4d7cb899d0882759444826d0c4c7d4e5c2220a9
HAC22 <deampuleadd@yandex.com>
2023-03-05 22:19:46 +0100

    [CHANGES] sidebar shadows to elevate the sidebar

--- resources/views/components/sidebar/sidebar.blade.php
+++ resources/views/components/sidebar/sidebar.blade.php
@@ -1,7 +1,7 @@
 <x-sidebar.overlay />
 
 <aside
-    class="fixed inset-y-0 z-20 flex flex-col py-4 space-y-6 shadow-lg dark:bg-dark-eval-1 bg-gray-50"
+    class="fixed inset-y-0 z-20 flex flex-col py-4 space-y-6 shadow-lg shadow-neutral-400 dark:shadow-neutral-900 dark:bg-dark-eval-1 bg-gray-50"
     :class="{
         'translate-x-0 w-64': isSidebarOpen || isSidebarHovered,
         '-translate-x-full w-64 md:w-16 md:translate-x-0': !isSidebarOpen && !isSidebarHovered,
